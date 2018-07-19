9d77ad6ae090d9e832d84e47ca4eb4a383417edb
Nicolai Bonde <neobonde@gmail.com>
2018-07-19 20:19:03 +0200

    better commets for util.py

--- util.py
+++ util.py
@@ -13,15 +13,21 @@ def printHelpOutput():
     print(output)
 
 def printVersionOutput():
-    print("\nUtilities " + VERSION)
+    # Print util's version, manually set in the top of the file
+    print("\nutil " + VERSION)
     
+    # Print branch name
     print("    Branch: " + str(check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"]).decode('utf-8')[:-1]))
     
+    # Check if repo is clean
     postHash = ''
     if str(check_output(["git", "diff", "--stat"]).decode('utf-8')) != '':
         postHash = '-dirty'
+    
+    # Print git hash and if repo is dirty 
     print("    Hash: " + str(check_output(["git", "rev-parse", "--short", "HEAD"]).decode('utf-8')[:-1] + postHash))
     
+    # Print the lastest commit message
     print("    Commit Message: " + str(check_output(["git", "log", "-1", "--pretty=%B"]).decode('utf-8')[:-2])) # Commit messages are apperently have an extra newline?
     print("")
 
@@ -31,14 +37,24 @@ if __name__ == "__main__":
         # Handle GUI
         if sys.argv[1] in ["-g", "--gui"]:
             GUIRunner(GUI("Utilities"))
+        
+        # Handle help 
         elif sys.argv[1] in ["-h","--help"]:
             print("")
             printHelpOutput()
+        
+        # Handle version
         elif sys.argv[1] in ["-v","--version"]:
            printVersionOutput()
+
+        # Handle unknown command
         else:
             print("")
+            
+            # Print unkown command passed to util
             print("Unkown option: " + sys.argv[1])
+
+            # Print help output to help user figure out what to do 
             printHelpOutput()
     else:
         # Handle CLI
